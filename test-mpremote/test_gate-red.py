1fae16e57bcd1d2e7ddb00eea9bc6729e21b537d
Edward Vidal Jr <develone@sbcglobal.net>
2025-04-26 08:52:12 -0600

    testing

---
+++ test-mpremote/test_gate-red.py
@@ -0,0 +1,7 @@
+from machine import Pin
+import ice
+file = open("gateware-red.bin","br")
+flash = ice.flash(miso=Pin(4), mosi=Pin(7), sck=Pin(6), cs=Pin(5))
+flash.write(file)
+fpga = ice.fpga(cdone=Pin(40), clock=Pin(21), creset=Pin(31), cram_cs=Pin(5), cram_mosi=Pin(4), cram_sck=Pin(6), frequency=48)
+fpga.start()
