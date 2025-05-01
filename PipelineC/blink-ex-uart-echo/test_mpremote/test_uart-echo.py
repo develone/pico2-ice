c00386a43c8cb96ce60fe0d1f18b3e18f1c4bbc4
Edward Vidal Jr <develone@sbcglobal.net>
2025-05-01 10:55:55 -0600

    gateware.bin tp uart-echo-PipelineC shasum uart-echo-PipelineC.bin 913ec9ebc46872eb61cc2cb86b9318fe3007c76b  uart-echo-PipelineC.bin

--- PipelineC/blink-ex-uart-echo/test_mpremote/test_uart-echo.py
+++ PipelineC/blink-ex-uart-echo/test_mpremote/test_uart-echo.py
@@ -2,10 +2,16 @@ from machine import Pin
 from machine import UART
 import ice
 #frequency=12 since a pll is used to increase to 25MHz 
-fpga = ice.fpga(cdone=Pin(40), clock=Pin(21), creset=Pin(31), cram_cs=Pin(5), cram_mosi=Pin(4), cram_sck=Pin(6), frequency=48)
+fpga = ice.fpga(cdone=Pin(40), clock=Pin(21), creset=Pin(31), cram_cs=Pin(5), cram_mosi=Pin(4), cram_sck=Pin(6), frequency=12)
 file = open("uart-echo-PipelineC.bin","br")
 flash = ice.flash(miso=Pin(4), mosi=Pin(7), sck=Pin(6), cs=Pin(5))
 fpga.start()
 fpga.cram(file)
 uart = UART(0, 115200)
-uart.init(115200, bits=8, parity=None, stop=1, tx=0, rx=1) # init with given parameters
+uart.init(115200, bits=8, parity=None, stop=1, tx=28, rx=29) # init with given parameters
+bufout = "abc"
+print(bufout)
+uart.write(bufout)
+uart.readinto(bufin,3)
+print(bufin)
+
