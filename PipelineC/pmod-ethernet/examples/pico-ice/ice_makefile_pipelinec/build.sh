07aee6d0337f498b9d78cd373e27885a54869571
Edward Vidal Jr <develone@sbcglobal.net>
2025-05-10 19:48:40 -0600

    make clean all BOARD=PICO2 PIPELINEC_TOP_FILE=ethernet_top.c PLL_CLK_MHZ=16.0 NEXTPNR_ARGS="--pre-pack eth_clocks.py" if test -e "gateware.bin"; then         echo "gateware.bin exits"         rm -f /media/devel/5221-000/*.bin         cp gateware.bin /media/devel/5221-0000/         date         ls -la /media/devel/5221-0000         mpremote run pico2_program_ice.py         mpremote run  test_uart-a-z.py         cd ../../net         ./compile-work_test.sh         sudo ./work_test         cd ../pico-ice/ice_makefile_pipelinec/ fi

--- PipelineC/pmod-ethernet/examples/pico-ice/ice_makefile_pipelinec/build.sh
+++ PipelineC/pmod-ethernet/examples/pico-ice/ice_makefile_pipelinec/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-make clean
-make pipelinec TOP_NAME=ethernet_top  NEXTPNR_ARGS="--pre-pack eth_clocks.py"
-make gateware.bin TOP_NAME=ethernet_top  NEXTPNR_ARGS="--pre-pack eth_clocks.py"
-bin2uf2 -o gateware.uf2 gateware.bin
+make clean all BOARD=PICO2 PIPELINEC_TOP_FILE=ethernet_top.c PLL_CLK_MHZ=16.0 NEXTPNR_ARGS="--pre-pack eth_clocks.py"
+if test -e "gateware.bin"; then
+	echo "gateware.bin exits"
+	rm -f /media/devel/5221-000/*.bin
+	cp gateware.bin /media/devel/5221-0000/
+	date
+	ls -la /media/devel/5221-0000
+	mpremote run pico2_program_ice.py
+	mpremote run  test_uart-a-z.py
+	cd ../../net
+	./compile-work_test.sh
+	sudo ./work_test
+	cd ../pico-ice/ice_makefile_pipelinec/ 
+fi
